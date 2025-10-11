6dcb0793900332f0443b88d1e5f9a9f30236c880
Jeff-online <94619758+Jeff-online@users.noreply.github.com>
2025-10-11 12:27:36 +0900

    Delete app directory

--- app/system/__init__.py
+++
@@ -1,8 +0,0 @@
-from app import messages
-from flask import Blueprint
-from flask_restful import Api
-
-wsystem = Blueprint("wsystem", __name__)
-system_api = Api(wsystem, errors=messages.errors)
-
-from . import user, homepage
--- app/user_auth/__init__.py
+++
@@ -1,8 +0,0 @@
-from app import messages
-from flask import Blueprint
-from flask_restful import Api
-
-wuser = Blueprint("wuser", __name__)
-wuser_api = Api(wuser, errors=messages.errors)
-
-from . import log_out
